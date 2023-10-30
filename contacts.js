41c4b7665e92731434233570ec874e3037dc51e3
Yakimenko Viktor <60235171+chell-iz-naroda@users.noreply.github.com>
2023-10-31 01:48:20 +0200

    node-js-01-hw-v-1-0-1

--- contacts.js
+++ contacts.js
@@ -15,9 +15,10 @@ async function listContacts() {
 async function getContactById(contactId) {
     // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
-    const contact = contacts.find((contact) => contact.id = contactId);
 
-    return contact;
+    const contact = contacts.find((contact) => contact.id === contactId);
+
+    return contact === undefined ? null : contact;
 }
 
 async function removeContact(contactId) {
@@ -27,12 +28,14 @@ async function removeContact(contactId) {
     const index = contacts.findIndex((contact) => contact.id === contactId);
 
     if (index === -1) {
-        return console.warn('\x1B[31m Non-existent id!');
+        return '\x1B[31mNon-existent id!\n' + null;
     }
 
     const newContacts = [...contacts.slice(0, index), ...contacts.slice(index + 1)];
 
-    return fs.writeFile(contactsPath, JSON.stringify(newContacts, undefined, 2));
+    fs.writeFile(contactsPath, JSON.stringify(newContacts, undefined, 2));
+
+    return contacts[index];
 }
 
 async function addContact(name, email, phone) {
@@ -44,6 +47,8 @@ async function addContact(name, email, phone) {
     contacts.push(newBook);
 
     fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+
+    return newBook;
 }
 
 module.exports = {
